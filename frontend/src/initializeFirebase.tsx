d69074e64e74bebacf1d4b5b711c564df868fdee
Ellie <elliem40499@gmail.com>
2025-03-26 21:22:56 -0400

    updated config and initalized firebase connection to the frontend. updated frontend packages to include firebase

--- frontend/src/initializeFirebase.tsx
+++ frontend/src/initializeFirebase.tsx
@@ -1,6 +1,7 @@
-import pyrebase
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from "firebase/database";
 
-config = {
+const firebaseConfig = {
   'apiKey': "AIzaSyAPXpHYZpD5DNjsg6XAEhTVA5V9kA5rNWY",
   'authDomain': "flashcards-93088.firebaseapp.com",
   'databaseURL': "https://flashcards-93088-default-rtdb.firebaseio.com",
@@ -10,4 +11,5 @@ config = {
   'appId': "457073208752:web:0c6e9eaea7740d4a387a75"
 }
 
-firebase = pyrebase.initialize_app(config)
+export const app = initializeApp(firebaseConfig)
+export const db = getDatabase(app)
