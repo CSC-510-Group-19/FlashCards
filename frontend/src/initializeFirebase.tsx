60567eaf5eadf27a310b4413b7f38a898f3d82b8
Ellie <elliem40499@gmail.com>
2025-03-31 13:31:12 -0400

    updated config and initalized firebase connection to the frontend. updated frontend packages to include firebase

--- frontend/src/initializeFirebase.tsx
+++ frontend/src/initializeFirebase.tsx
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   'apiKey': "AIzaSyAPXpHYZpD5DNjsg6XAEhTVA5V9kA5rNWY",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig)
 export const db = getDatabase(app)
+export const auth = getAuth()
